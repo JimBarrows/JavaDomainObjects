b21bf157976500ce90cbc9e58678d23cbed717f5
Jim Barrows <Jim.Barrows@gmail.com[color]>
2011-02-09 23:40:41 -0700

    reformatted

--- web-interface/src/main/webapp/js/bizondemand/party/PartyForm.js
+++ web-interface/src/main/webapp/js/bizondemand/party/PartyForm.js
@@ -1,89 +1,86 @@
 Ext.namespace('BizOnDemand.Party');
 BizOnDemand.Party.PartyForm = Ext.extend(Ext.form.FormPanel, {
     
-		id: 'BizOnDemand.Party.PartyForm'
-		,title: 'Party Form'
-    ,url: 'business.json'
-		,waitMessage: 'Please wait.'
-    ,initComponent: function() {
-        var config = {
-        		items: [{
-        			xtype:'textfield'
-        			,id: 'BizOnDemand.Party.PartyForm.NameField'
-        			,fieldLabel: 'Name'
-        			,allowBlank:false
-        		}]
-        		,buttons: [{
-      				text:'Submit'
-      				,formBind:true
-      				,scope:this
-      				,handler: this.submit 
-      			}]
-        };
+	id: 'BizOnDemand.Party.PartyForm'
+  ,url: 'business.json'
+	,waitMessage: 'Please wait.'
+  ,initComponent: function() {
+  	var config = {
+    	items: [{
+      	xtype:'textfield'
+        ,id: 'BizOnDemand.Party.PartyForm.NameField'
+        ,fieldLabel: 'Name'
+        ,allowBlank:false
+      }]
+      ,buttons: [{
+      	text:'Submit'
+      	,formBind:true
+      	,scope:this
+      	,handler: this.submit 
+      }]
+    };
 
-        // apply config
-        Ext.apply(this, config);
-        Ext.apply(this.initialConfig, config);
+    // apply config
+    Ext.apply(this, config);
+    Ext.apply(this.initialConfig, config);
 
-        BizOnDemand.Party.PartyForm.superclass.initComponent.apply(this, arguments);
+    BizOnDemand.Party.PartyForm.superclass.initComponent.apply(this, arguments);
 
-        // after parent code here, e.g. install event handlers
-        // this.on('beforerender', function(dis) {
-        // alert('before render');
-        // });
-    }
+    // after parent code here, e.g. install event handlers
+    // this.on('beforerender', function(dis) {
+    // alert('before render');
+    // });
+  }
     
-    ,onRender: function() {
-
-        BizOnDemand.Party.PartyForm.superclass.onRender.apply(this, arguments);
-	
+  ,onRender: function() {
+  	BizOnDemand.Party.PartyForm.superclass.onRender.apply(this, arguments);
 	}
 	
-,submit : function(button, event) {
-    	this.getForm().submit({
-    		url: this.url
-    		,scope: this
-    		,waitMsg: this.waitMsg
-    		,success: this.onSuccess
-    		,failure: this.onFailure
-    	})
-    }
+	,submit : function(button, event) {
+		this.getForm().submit({
+    	url: this.url
+    	,scope: this
+    	,waitMsg: this.waitMsg
+    	,success: this.onSuccess
+    	,failure: this.onFailure
+    })
+  }
     
-    ,onSuccess: function(form, action) {
-    	Ext.Msg.show({
-            title: 'Success'
-            ,msg: 'Success!'
-            ,modal: true
-            ,icon: Ext.Msg.INFO
-            ,buttons: Ext.Msg.OK            
-        });
-    }
+  ,onSuccess: function(form, action) {
+  	Ext.Msg.show({
+    	title: 'Success'
+      ,msg: 'Success!'
+      ,modal: true
+      ,icon: Ext.Msg.INFO
+      ,buttons: Ext.Msg.OK            
+    });
+  }
     
-    ,onFailure: function(form, action) {
+  ,onFailure: function(form, action) {
 		switch (action.failureType) {
-	    	case Ext.form.Action.CLIENT_INVALID:
-	            this.onClientInvalid( form,action);
-	        break;
-	        case Ext.form.Action.CONNECT_FAILURE:
-	            this.onConnectionFailure(form,action);
-	        break;
-	        case Ext.form.Action.SERVER_INVALID:
-	        	this.onServerInvalid(form,action);
-	        break;	        
-        };
-    }
+	  	case Ext.form.Action.CLIENT_INVALID:
+	    	this.onClientInvalid( form,action);
+	    break;
+	    case Ext.form.Action.CONNECT_FAILURE:
+	    	this.onConnectionFailure(form,action);
+	    break;
+	    case Ext.form.Action.SERVER_INVALID:
+	     	this.onServerInvalid(form,action);
+	    break;	        
+    };
+  }
     
-    ,onClientInvalid: function(form,action) {
-   		Ext.Msg.alert('Failure', 'Form fields may not be submitted with invalid values'); 	
-    }
+  ,onClientInvalid: function(form,action) {
+  	Ext.Msg.alert('Failure', 'Form fields may not be submitted with invalid values'); 	
+  }
     
-    ,onConnectionFailure: function(form,action) {
-    	Ext.Msg.alert('Failure', 'Ajax communication failed');
-    }
+  ,onConnectionFailure: function(form,action) {
+   	Ext.Msg.alert('Failure', 'Ajax communication failed');
+  }
     
-    ,onServerInvalid: function(form,action) {
-    	Ext.Msg.alert('Failure', action.result.msg);
-    }
+  ,onServerInvalid: function(form,action) {
+   	Ext.Msg.alert('Failure', action.result.msg);
+  }
 });
 
 Ext.reg('BizOnDemand.Party.PartyForm', BizOnDemand.Party.PartyForm);
