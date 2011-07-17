bc7e24cbdbf18be60ca3d95d9d6d92250cf52b2e
Jim Barrows <Jim.Barrows@gmail.com[color]>
2011-07-17 12:52:16 -0700

    removing scala stuff

--- accounting-model/src/main/scala/com/bizondemand/biz360/budgeting/model/Budget.scala
+++
@@ -1,9 +0,0 @@
-package com.bizondemand.biz360.budgeting.model
-
-class Budget(id: Long, comment:String, timePeriod:StandardTimePeriod) {
-	timePeriod.add( this);
-}
-
-class OperatingBudget (id:Long, comment:String, timePeriod:StandardTimePeriod) extends Budget(id, comment, timePeriod)
-
-class CaptialBudget (id:Long, comment:String, timePeriod:StandardTimePeriod) extends Budget(id,comment, timePeriod:StandardTimePeriod)
--- web-interface/src/main/scala/code/model/Model.scala
+++
@@ -1,7 +0,0 @@
-package code.model 
-
-import _root_.org.scala_libs.jpa.LocalEMF
-import _root_.net.liftweb.jpa.RequestVarEM
-
-object Model extends LocalEMF("jpaweb") with RequestVarEM
-
