68d9f85e2246b8ea9e982f5ebbb55eaf70d39da2
Jim Barrows <Jim.Barrows@gmail.com>
2015-07-14 17:49:36 -0700

    Nuked uneeded import

--- WorkEffort/src/main/java/jdo/workeffort/model/WorkEffortStatus.java
+++ WorkEffort/src/main/java/jdo/workeffort/model/WorkEffortStatus.java
@@ -1,12 +1,9 @@
 package jdo.workeffort.model;
 
 import java.time.ZonedDateTime;
-import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import jdo.model.BasePersistentModel;
 
