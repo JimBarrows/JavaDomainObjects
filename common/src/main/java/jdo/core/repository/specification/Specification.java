71b0149cef4683ce5839ea0fac7c8d6e4dcb98cf
Jim Barrows <JIm.Barrows@gmail.com>
2015-07-29 15:20:37 -0700

    Forgot these methods I think

--- common/src/main/java/jdo/core/repository/specification/Specification.java
+++ common/src/main/java/jdo/core/repository/specification/Specification.java
@@ -3,4 +3,16 @@ package jdo.core.repository.specification;
 public interface Specification<E> {
 
 	public boolean isSatisfiedBy( E entity);
+	
+  	public Specification<E> and(final Specification<E> specification) {
+    		return new AndSpecification<T>(this, specification);
+  	}
+
+	public Specification<E> or(final Specification<E> specification) {
+    		return new OrSpecification<E>(this, specification);
+  	}
+  	
+	public Specification<E> not(final Specification<E> specification) {
+    		return new NotSpecification<E>(specification);
+  	}
 }
