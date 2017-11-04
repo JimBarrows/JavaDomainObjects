0c1bb186611210528174bf43c26ee3cb9156186b
Jim Barrows <jim.barrows@gmail.com>
2017-11-04 09:43:40 -0500

    Refactored the appliation configuration class to use the repo.  Uncommented out the maven configuration to generate ddl.

--- application/src/main/java/jdo/application/ApplicationConfiguration.java
+++ application/src/main/java/jdo/application/ApplicationConfiguration.java
@@ -1,45 +1,31 @@
 package jdo.application;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
-import java.util.stream.Collectors;
-
-import javax.ejb.Stateful;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.PersistenceContextType;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-
 import jdo.party.model.Company;
-import jdo.party.model.PartyRole;
+import jdo.party.model.Party;
 import jdo.party.model.roles.InternalOrganization;
 import jdo.party.model.roles.ParentOrganization;
+import jdo.party.repositories.PartyRepository;
+
+import javax.ejb.EJB;
+import javax.ejb.Stateful;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import static jdo.party.specifications.PartyHasActiveRoleOf.partyHasActiveRoleOf;
 
 /**
  * Single place to get all configuration information, without having to worry
  * about where it comes from.
  *
  * @author Jim
- *
  */
 @Stateful
 public class ApplicationConfiguration {
 
-	/**
-	 * Entity manager for jpa2 access.
-	 */
-	@PersistenceContext(name = "PeopleAndOrganizations", type = PersistenceContextType.EXTENDED)
-	private EntityManager entityManager;
+	@EJB
+	private PartyRepository partyRepository;
 
-	public ApplicationConfiguration(final EntityManager entityManager) {
-		super();
-		this.entityManager = entityManager;
-	}
 
 	public ApplicationConfiguration() {
 		super();
@@ -62,8 +48,7 @@ public class ApplicationConfiguration {
 		company.setName("The company in question");
 		company.addPartyRole(new InternalOrganization());
 		company.addPartyRole(new ParentOrganization());
-		entityManager.persist(company);
-		return company;
+		return (Company) partyRepository.create(company);
 	};
 
 	/**
@@ -71,47 +56,11 @@ public class ApplicationConfiguration {
 	 *
 	 * @return The company, or empty if we can't find it.
 	 */
-	public Optional<Company> findCompany() {
-		final CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-		final CriteriaQuery<Company> criteria = builder.createQuery(Company.class);
-		final Root<Company> entityRoot = criteria.from(Company.class);
-		criteria.select(entityRoot);
+	protected Optional<Company> findCompany() {
+		final List<Party> filteredList = partyRepository.findBy(partyHasActiveRoleOf(InternalOrganization.class).and(partyHasActiveRoleOf(ParentOrganization.class)));
 
-		final TypedQuery<Company> query = entityManager.createQuery(criteria);
-		final List<Company> list = query.getResultList();
-		// I couldn't figure out the freaking JPA criteria.. this was easier :)
-		// TODO Figure out how to do this in a criteria
-
-		final List<Company> filteredList = list.stream().filter(c -> {
-			final boolean hasParentOrganization = c.getActingAs().stream().anyMatch(parentOrganizationPredicate());
-			final boolean hasInternalOrganization = c.getActingAs().stream().anyMatch(internalOrganizationPredicate());
-			return hasParentOrganization && hasInternalOrganization;
-		}).collect(Collectors.toList());
-		return filteredList.isEmpty() ? Optional.empty() : Optional.ofNullable(filteredList.get(0));
+		return filteredList.isEmpty() ? Optional.empty() : Optional.ofNullable((Company) filteredList.get(0));
 
 	}
 
-	/**
-	 * Convenience method for an anonymous function to filter for the
-	 * internalOrganization role.
-	 *
-	 * @return an anonymous function that filters for the internal organization
-	 *         role.
-	 */
-	public static final Predicate<PartyRole> internalOrganizationPredicate() {
-		final Predicate<PartyRole> isInternalOrganization = a -> (a instanceof InternalOrganization)
-				& a.getDateTimeRange().isActive();
-		return isInternalOrganization;
-	}
-
-	/**
-	 * Convenience method for an anonymous function to filter for the parent
-	 * organization role.
-	 *
-	 * @return and anonymous function that filters for parent organization role.
-	 */
-	public static final Predicate<PartyRole> parentOrganizationPredicate() {
-		final Predicate<PartyRole> isParent = a -> (a instanceof ParentOrganization);
-		return isParent;
-	}
 }
